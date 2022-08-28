0f4703e93725941ea79ec63d7a4527bf5c7ae27f
Juliette Cordor <grassojames5@gmail.com>
2022-08-28 15:08:04 +1000

    added basic base64 example

--- src/tools/base64encode.rs
+++ src/tools/base64encode.rs
@@ -1,23 +1,46 @@
 use yew::prelude::*;
 
-use crate::ffi::log;
-
 #[function_component(Encode)]
 pub fn encode() -> Html {
     let input = use_state(|| "".to_string());
+    let output = use_state(|| "".to_string());
+    let input_ref = use_ref(NodeRef::default);
 
-    let set_input = {
+    let input_changed = {
         let input = input.clone();
-
-        move |v: Event| {
-            input.set(v.as_string().unwrap_or_default());
-        }
+        let input_ref = input_ref.clone();
+        Callback::from(move |_| {
+            input.set(
+                input_ref
+                    .cast::<web_sys::HtmlInputElement>()
+                    .unwrap()
+                    .value(),
+            );
+        })
     };
+
+    {
+        let input_ref = input.clone();
+        let output = output.clone();
+
+        use_effect_with_deps(
+            move |_| {
+                let input_value = &*input_ref;
+
+                let output_value = base64::encode(input_value);
+
+                output.set(output_value);
+
+                || {}
+            },
+            [input],
+        );
+    }
 
     html! {
         <div class="container">
-            <input label="Input" onchange={Callback::from(set_input)} />
-            <p>{ &*input }</p>
+            <input type="text" label="Input" ref={&*input_ref} oninput={input_changed} />
+            <p>{ &*output }</p>
         </div>
     }
 }
