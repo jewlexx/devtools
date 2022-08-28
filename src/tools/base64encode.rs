eed7a6c605155b9dd50415c4557bbbe11c4232bb
Juliette Cordor <grassojames5@gmail.com>
2022-08-28 14:56:21 +1000

    fix

--- src/tools/base64encode.rs
+++ src/tools/base64encode.rs
@@ -1,13 +1,23 @@
-use material_yew::text_inputs::MatTextField;
 use yew::prelude::*;
 
 use crate::ffi::log;
 
 #[function_component(Encode)]
 pub fn encode() -> Html {
+    let input = use_state(|| "".to_string());
+
+    let set_input = {
+        let input = input.clone();
+
+        move |v: Event| {
+            input.set(v.as_string().unwrap_or_default());
+        }
+    };
+
     html! {
         <div class="container">
-            <MatTextField outlined=true label="Input" oninput={Callback::from(|v: String| {log(&v);})} />
+            <input label="Input" onchange={Callback::from(set_input)} />
+            <p>{ &*input }</p>
         </div>
     }
 }
