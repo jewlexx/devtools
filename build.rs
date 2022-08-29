8e73c37404d7abafc9eea112c96e80911a3f920b
Juliette Cordor <grassojames5@gmail.com>
2022-08-29 20:06:30 +1000

    updated build script

--- build.rs
+++ build.rs
@@ -5,8 +5,13 @@ fn main() {
     let target_path = current_path.join("src/styles/blueprint.css");
     yewprint_css::download_css(&target_path).unwrap();
 
+    // If running in debug build mode, no need to minify css
+    if cfg!(debug_assertions) {
+        return;
+    }
+
     let blueprint_css = {
-        let mut blueprint_css_file = File::open(target_path).unwrap();
+        let mut blueprint_css_file = File::open(&target_path).unwrap();
 
         let mut contents = vec![];
 
@@ -17,7 +22,7 @@ fn main() {
 
     let css_minified = minifier::css::minify(&blueprint_css).unwrap();
 
-    let target_file = File::create(current_path.join("src/styles/blueprint.min.css")).unwrap();
+    let target_file = File::create(&target_path).unwrap();
 
     css_minified.write(target_file).unwrap();
 }
