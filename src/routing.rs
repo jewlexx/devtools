faadbae4ddd7bc37dd5097e9a95c30b97bff7fe2
Juliette Cordor <grassojames5@gmail.com>
2022-08-29 20:37:19 +1000

    fix

--- src/routing.rs
+++ src/routing.rs
@@ -1,6 +1,13 @@
 use strum::EnumIter;
 use yew_router::prelude::*;
 
+pub struct AppRoute<T: yew::FunctionProvider + 'static> {
+    pub route: Route,
+    pub title: &'static str,
+    pub description: &'static str,
+    pub component: yew::functional::FunctionComponent<T>,
+}
+
 #[derive(Clone, Routable, PartialEq, Eq, EnumIter)]
 pub enum Route {
     #[at("/")]
@@ -10,4 +17,8 @@ pub enum Route {
     #[not_found]
     #[at("/404")]
     NotFound,
+}
+
+impl<T: yew::FunctionProvider + 'static> From<Route> for AppRoute<T> {
+    fn from(route: Route) -> Self {}
 }
