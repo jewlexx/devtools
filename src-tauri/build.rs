e152694a159f80ee4e3103374b2f74ffc9c4c9fb
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-08-29 03:17:39 +0000

    fix

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,5 +1,3 @@
 fn main() {
-  let current_path = std::env::current_dir().unwrap();
-  yewprint_css::download_css(current_path.join("public/blueprint.css"));
-  tauri_build::build()
+    tauri_build::build()
 }
