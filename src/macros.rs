752811c5270e4404382fdfb4f24c558041c4e53e
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-08-30 00:39:39 +0000

    fix

--- src/macros.rs
+++ src/macros.rs
@@ -1,8 +1,6 @@
 #[macro_export]
 macro_rules! jsv {
     ($($tt:tt)*) => {
-        wasm_bindgen::JsValue {
-            $($tt)*
-        }
+        wasm_bindgen::JsValue::from_serde($($tt)*)
     };
 }
