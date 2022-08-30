5c4d5964c22381d36a0464542f316256a83a2f62
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-08-30 01:12:43 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,23 @@ mod not_found;
 mod routing;
 mod tools;
 
+use std::ops::Add;
+
 use app::App;
+
+struct U4 {
+    inner: [bool; 4],
+}
+
+impl Add<u8> for U4 {
+    type Output = U4;
+
+    fn add(self, rhs: u8) -> Self::Output {
+        if rhs > 15 {
+            panic!("U4 overflow");
+        }
+    }
+}
 
 fn main() {
     yew::start_app::<App>();
