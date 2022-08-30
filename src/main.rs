b26670aca1483ebbb0fc6107d5c0183c3cbafe23
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-08-30 01:13:26 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,20 @@ struct U4 {
     inner: [bool; 4],
 }
 
+impl Into<U4> for u8 {
+    fn into(self) -> U4 {
+        if self > 15 {
+            panic!("U4 overflow");
+        }
+
+        let mut inner = [false; 4];
+        for i in 0..4 {
+            inner[i] = self & (1 << i) != 0;
+        }
+        U4 { inner }
+    }
+}
+
 impl Add<u8> for U4 {
     type Output = U4;
 
