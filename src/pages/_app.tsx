0486c80bd404a19513508cb584858f94bdb2c7b7
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-08-31 15:18:11 +1000

    fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import 'normalize.css/normalize.css';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
