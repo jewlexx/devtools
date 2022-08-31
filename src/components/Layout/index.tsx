9cd789f4c574f0612aca1024e1b9ee2680167208
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-08-31 15:14:10 +1000

    fix

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,8 +4,10 @@ import styles from './index.module.scss';
 const Layout: FunctionComponent<{
   children: ReactNode;
 }> = ({ children }) => {
-  return 
-  <div className={styles.layout}><main className={styles.container}>{children}</main></div>;
+  return;
+  <div className={styles.layout}>
+    <main className={styles.container}>{children}</main>
+  </div>;
 };
 
 export default Layout;
