798869f7bdfe948eb9f457efcd3b77a1d7a002dd
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-08-31 14:21:54 +1000

    fix

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,8 @@
+import type { FunctionComponent } from 'react';
+import styles from './index.scss';
+
+const Layout: FunctionComponent = ({ children }) => {
+  return <>{children}</>;
+};
+
+export default Layout;
