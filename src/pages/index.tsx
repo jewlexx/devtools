f839972c5957d799f466e44fd99204ab0118fd4c
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-09-01 09:55:46 +1000

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import type { NextPage } from 'next';
 import Link from 'next/link';
 import Head from 'next/head';
 import Image from 'next/image';
-import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
