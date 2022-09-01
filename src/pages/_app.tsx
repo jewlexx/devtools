1b5273de22dedc34bd274c4134def26632071f41
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-09-01 10:10:15 +1000

    fixed issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,34 @@
 import type { AppProps } from 'next/app';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import 'normalize.css/normalize.css';
+import { createGlobalStyle } from 'styled-components';
 import { ColorModeProvider } from '../context/colourMode';
-import useColourMode from '../hooks/useColourMode';
+import useColourMode, { ColourMode } from '../hooks/useColourMode';
 import '../styles/globals.css';
+import { useMemo } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const colourMode = useColourMode();
 
+  const GlobalStyle = useMemo(() => {
+    if (colourMode === ColourMode.Dark) {
+      return createGlobalStyle`
+        html {
+          color-scheme: dark;
+        }
+        body {
+          color: white;
+          background: black;
+        }
+      `;
+    } else {
+      return createGlobalStyle``;
+    }
+  }, [colourMode]);
+
   return (
     <ColorModeProvider value={colourMode}>
+      <GlobalStyle />
       <Component {...pageProps} />
     </ColorModeProvider>
   );
