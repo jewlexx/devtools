17e7f0c478953db52544ad7559fb2f9144f18ba3
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-09-02 10:40:43 +1000

    added another route

---
+++ src/pages/tools/js-compiler.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import initSwc, { transformSync } from '@swc/wasm-web';
+
+const JsCompiler: NextPage = () => {
+  return <></>;
+};
+
+export const title = 'Js Compiler';
+export const description = 'Compile javascript';
+
+export default JsCompiler;
