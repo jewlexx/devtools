fe3391e37dec4b9465e0fef5f9dee170dd18a962
Juliette Cordor <61040004+jewlexx@users.noreply.github.com>
2022-09-02 10:42:41 +1000

    fix

--- src/pages/tools/js-compiler.tsx
+++ src/pages/tools/js-compiler.tsx
@@ -1,8 +1,31 @@
 import type { NextPage } from 'next';
+import { useEffect, useState } from 'react';
 import initSwc, { transformSync } from '@swc/wasm-web';
 
 const JsCompiler: NextPage = () => {
-  return <></>;
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    async function importAndRunSwcOnMount() {
+      await initSwc();
+      setInitialized(true);
+    }
+    importAndRunSwcOnMount();
+  }, []);
+
+  function compile() {
+    if (!initialized) {
+      return;
+    }
+    const result = transformSync(`console.log('hello')`, {});
+    console.log(result);
+  }
+
+  return (
+    <div className="App">
+      <button onClick={compile}>Compile</button>
+    </div>
+  );
 };
 
 export const title = 'Js Compiler';
